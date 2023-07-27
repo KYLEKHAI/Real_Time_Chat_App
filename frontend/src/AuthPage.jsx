0957dd47367f28950d817339bce80003a102ea12
Kyle Khai Tran <kylekhai04@gmail.com>
2023-07-27 18:52:28 -0400

    Use react-chat-engine-pretty to incorporate into ChatsPage

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -1,11 +1,15 @@
 //Set up an html form that takes username as input and button to submit
 //Sets user state
+import axios from "axios";
 
 const AuthPage = (props) => {
   const onSubmit = (e) => {
     e.preventDefault();
     const { value } = e.target[0];
-    props.onAuth({ username: value, secret: value });
+    axios
+      .post("http://localhost:3001/authenticate", { username: value })
+      .then((r) => props.onAuth({ ...r.data, secret: value }))
+      .catch((e) => console.log("error", e));
   };
 
   return (
--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,16 @@
-const ChatsPage = () => {
-  return <>chats...</>;
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <PrettyChatWindow
+        projectId={"80a67d79-6719-4f91-8f22-6d6d6309af97"}
+        username={props.user.username} // Kyle
+        secret={props.user.secret} // pass1234
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
 };
 
 export default ChatsPage;
