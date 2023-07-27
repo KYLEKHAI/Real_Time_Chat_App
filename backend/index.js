7b60f2aad96aa4e64d63c0a775740e80679fc939
Kyle Khai Tran <kylekhai04@gmail.com>
2023-07-27 18:06:09 -0400

    Create user & keys and return specified data

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,25 @@
-//Run independencies
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
-//Takes username from request body and returns fake user with password
+//Get user name and create/destroy users --> return specified data or error
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "e41d596d-52b5-421e-a3a2-27d64a84dcad " } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
 });
 
 app.listen(3001);
